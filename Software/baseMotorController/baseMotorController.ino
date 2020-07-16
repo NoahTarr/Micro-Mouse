e2fd295506133b2648e71b0a6450c5bbc0204889
Noah Tarr <noahtarr1@gmail.com>
2020-07-16 01:13:18 -0700

    New Motor Controller Base Sketch and gitIgnore changes

---
+++ Software/baseMotorController/baseMotorController.ino
@@ -0,0 +1,17 @@
+const int motorLeftForwardPin = 8; //Motor A - Ain1
+const int motorLeftBackPin = 9; //Motor A - Ain2
+const int motorRightForwardPin = 11; //Motor B - Bin1
+const int motorRightBackPin = 10; //Motor B - Bin2
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(motorLeftForwardPin, OUTPUT);
+  pinMode(motorLeftBackPin, OUTPUT);
+  pinMode(motorRightForwardPin, OUTPUT);
+  pinMode(motorRightBackPin, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+}
