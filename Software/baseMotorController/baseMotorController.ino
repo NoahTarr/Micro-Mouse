6cf0610276faf51b7f2984db0c43eb8e7483367d
Tyler F. Kelley <tfkelley@users.noreply.github.com>
2020-07-16 13:17:26 -0700

    Update baseMotorController.ino
    Forward , slow down, reverse

--- Software/baseMotorController/baseMotorController.ino
+++ Software/baseMotorController/baseMotorController.ino
@@ -13,5 +13,35 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+
+  //Code to start , slow down , brake, then reverse.
+delay(2000);
+
+//FORWARD
+analogWrite(motorLeftForwardPin , 255);
+analogWrite(motorLeftBackPin , 0);
+analogWrite(motorRightForwardPin , 255);
+analogWrite(motorRightBackPin , 0);
+
+delay(1000);
+
+//SLOW DOWN
+analogWrite(motorLeftForwardPin , 0);
+analogWrite(motorLeftBackPin , 0);
+analogWrite(motorRightForwardPin , 0);
+analogWrite(motorRightBackPin , 0);
+
+delay(8000);
+
+//REVERSE
+analogWrite(motorLeftForwardPin , 0);
+analogWrite(motorLeftBackPin , 255);
+analogWrite(motorRightForwardPin , 0);
+analogWrite(motorRightBackPin , 255);
+
+delay(2500);
+
+  
   
 }
