677319a792492f6810e7b2f613a525c7bf345411
Noah Tarr <noahtarr1@gmail.com>
2020-07-17 21:18:54 -0700

    Encoder Commit 2 - Potentially Broken\Power Issue

--- Software/baseMotorController/baseMotorController.ino
+++ Software/baseMotorController/baseMotorController.ino
@@ -1,119 +1,123 @@
-const int motorLeftForwardPin = 8; //Motor A - Ain1
-const int motorLeftBackPin = 9; //Motor A - Ain2
-const int motorRightForwardPin = 11; //Motor B - Bin1
-const int motorRightBackPin = 10; //Motor B - Bin2
+//pins for motors
+#define RH_MOTOR_Forward 11 //RH = A on motorController
+#define RH_MOTOR_BACK 10
+#define LH_MOTOR_Forward 8 //LH = B on motorController
+#define LH_MOTOR_BACK 9
 
 //pins for encoder inputs
-#define RH_ENCODER_A 7
-#define RH_ENCODER_B 6
-#define LH_ENCODER_A 4
-#define LH_ENCODER_B 3
+#define RH_ENCODER_A 2
+#define RH_ENCODER_B 4
+#define LH_ENCODER_A 3
+#define LH_ENCODER_B 5
+
+#define power 255
+#define maxPower 255
 
 // variables for rotation count
-volatile unsigned long LeftCount = 0;
-volatile unsigned long RightCount = 0;
+// Declared volatile because they are altered in ISR (Interrupt Service Routine)
+volatile int rightCount = 0;
+volatile int leftCount = 0;
 
-EnableInterrupts()
+//Function Declarations
+//void rightEncoderEvent();
+//void leftEncoderEvent();
 
 void setup() {
+  Serial.begin(9600); //Opens serial port and sets data range to 9600 bps
   
-  // put your setup code here, to run once:
-  pinMode(motorLeftForwardPin, OUTPUT);
-  pinMode(motorLeftBackPin, OUTPUT);
-  pinMode(motorRightForwardPin, OUTPUT);
-  pinMode(motorRightBackPin, OUTPUT);
+  pinMode(RH_MOTOR_Forward, OUTPUT);
+  pinMode(RH_MOTOR_BACK, OUTPUT);
+  pinMode(LH_MOTOR_Forward, OUTPUT);
+  pinMode(LH_MOTOR_BACK, OUTPUT);
 
- //Following is setup for the encoder
+  pinMode(RH_ENCODER_A, INPUT);
+  pinMode(RH_ENCODER_B, INPUT);
+  pinMode(LH_ENCODER_A, INPUT);
+  pinMode(LH_ENCODER_B, INPUT);
 
- pinMode(RH_ENCODER_A, INPUT);
- pinMode(RH_ENCODER_B, INPUT);
- pinMode(LH_ENCODER_A, INPUT);
- pinMode(LH_ENCODER_B, INPUT);
-
-//INTERRUPTS
-
-attachInterrupt(digitalPinToInterrupt(0), leftEncoderEvent, CHANGE);
-attachInterrupt(digitalPinToInterrupt(1), rightEncoderEvent, CHANGE);
-
-Serial.begin(9600); //Opens serial port and sets data range to 9600 bps
-
+  //INTERRUPTS
+//  attachInterrupt(digitalPinToInterrupt(RH_ENCODER_A), rightEncoderEvent, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(LH_ENCODER_A), leftEncoderEvent, CHANGE);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+  //Encoder looping code
+//  Serial.print("Right Count: ");
+//  Serial.println(rightCount);
+//  Serial.print("Left Count: ");
+//  Serial.println(leftCount);
+//  Serial.println();
+  
 
-//Encoder looping code
+  //BREAK
+  analogWrite(LH_MOTOR_Forward , maxPower);
+  analogWrite(LH_MOTOR_BACK , maxPower);
+  delay(1000);
+  
+  //FORWARD
+  analogWrite(LH_MOTOR_Forward , power);
+  analogWrite(LH_MOTOR_BACK , 0);
+  delay(1000);
 
- Serial.print("Right Count: ");
-  Serial.println(rightCount);
-  Serial.print("Left Count: ");
   Serial.println(leftCount);
-  Serial.println();
-  delay(500);
+  //BREAK
+  analogWrite(LH_MOTOR_Forward , maxPower);
+  analogWrite(LH_MOTOR_BACK , maxPower);
+  delay(1000);
+  
+  //REVERSE
+  analogWrite(LH_MOTOR_Forward , 0);
+  analogWrite(LH_MOTOR_BACK , power);
+  delay(1000);
+  
+  Serial.println(leftCount);
+  
+  //BREAK
+  analogWrite(LH_MOTOR_Forward , maxPower);
+  analogWrite(LH_MOTOR_BACK , maxPower);
+  delay(1000);
+  
+}
 
-//Interupt Call
 
-void LeftEncoderEvent() {
-if (digitalRead(LH_ENCODER_A) == HIGH) {
-  if(digitalRead(LH_ENCODER_B) == LOW) {
-    leftCount++;
-  } else {
-    LeftCount--;
-  }
-} else {
-    if (digitalRead(LH_ENCODER_B) == LOW) {
-      leftCount--;
-    } else {
+//void leftEncoderEvent() {
+//    if(digitalRead(LH_ENCODER_B) == LOW) { //Encoder moving Clockwise 
+//      leftCount++;
+//    } else { //Encoder moving Counter Clockwise 
+//      leftCount--;
+//    } 
+//}
+
+
+void leftEncoderEvent() {
+  if (digitalRead(LH_ENCODER_A) == HIGH) {
+    if(digitalRead(LH_ENCODER_B) == LOW) { //Encoder moving Clockwise 
       leftCount++;
-    }
-  }
-}
-
-void rightEncoderEvent() {
-  if (digitalRead(RH_ENCODER_A) == HIGH) {
-    if (digitalRead(RH_ENCODER_B) == LOW) {
-      rightCount++;
-    } else {
-      rightCount--;
-    }
+    } else { //Encoder moving Counter Clockwise 
+      leftCount--;
+    } 
   } else {
-    if (digitalRead(RH_ENCODER_B) == LOW) {
-      rightCount--;
-    } else {
-      rightCount++;
-    }
+      if (digitalRead(LH_ENCODER_B) == LOW) { //Encoder moving Clockwise 
+        leftCount--;
+      } else { //Encoder moving Counter Clockwise 
+        leftCount++;
+      }
   }
 }
 
-
-
-  //Code to start , slow down , brake, then reverse.
-delay(2000);
-
-//FORWARD
-analogWrite(motorLeftForwardPin , 255);
-analogWrite(motorLeftBackPin , 0);
-analogWrite(motorRightForwardPin , 255);
-analogWrite(motorRightBackPin , 0);
-
-delay(1000);
-
-//SLOW DOWN
-analogWrite(motorLeftForwardPin , 0);
-analogWrite(motorLeftBackPin , 0);
-analogWrite(motorRightForwardPin , 0);
-analogWrite(motorRightBackPin , 0);
-
-delay(8000);
-
-//REVERSE
-analogWrite(motorLeftForwardPin , 0);
-analogWrite(motorLeftBackPin , 255);
-analogWrite(motorRightForwardPin , 0);
-analogWrite(motorRightBackPin , 255);
-
-delay(2500);
-
-  
-  
-}
+//void rightEncoderEvent() {
+//  if (digitalRead(RH_ENCODER_A) == HIGH) {
+//    if (digitalRead(RH_ENCODER_B) == LOW) { 
+//      rightCount++;
+//    } else {
+//      rightCount--;
+//    }
+//  } else {
+//    if (digitalRead(RH_ENCODER_B) == LOW) {
+//      rightCount--;
+//    } else {
+//      rightCount++;
+//    }
+//  }
+//}
